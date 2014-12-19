7ed3c524ce1cd3d18fd079b2baf2809b1c0bfd7e
Andrew Reitz <aj.reitz@gmail.com>
2014-12-19 13:33:15 -0600

    Adding ability to choose library name
    Also minor fix for types in build.gradle

--- templates/android-lib/lazybones.groovy
+++ templates/android-lib/lazybones.groovy
@@ -1,4 +1,9 @@
 def props = [:]
+
+// settings.gradle
+props.libraryName = ask("What would you like your library to be called? ", "android-lib", "name")
+
+        // build.gradle
 // Lazybones tries to fine a value for $version if this is left in the build.gradle. This is to get it to stop complaining.
 props.versionString = "\$version\${project.hasProperty('release') ? '' : '-SNAPSHOT'}"
 props.minSdkVersion = ask("Define the minimum Android SDK version this library should support [7]: ", "7", "minSdkVersion")
