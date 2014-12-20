b39c16ce766f7503ea0fd0245358bcfa11437fd1
Andrew Reitz <aj.reitz@gmail.com>
2014-12-19 18:25:40 -0600

    adding replacments for gradle.properties.

--- templates/android-lib/lazybones.groovy
+++ templates/android-lib/lazybones.groovy
@@ -1,11 +1,24 @@
+import uk.co.cacoethes.util.NameType
+
 def props = [:]
 
 // settings.gradle
-props.libraryName = ask("What would you like your library to be called? ", "android-lib", "name")
 
-        // build.gradle
+def libraryName = ask("Define what this library will be called [android-lib]: ", "android-lib", "name")
+props.libraryName = transformText(libraryName, from: NameType.CAMEL_CASE, to: NameType.HYPHENATED)
+
+processTemplates "settings.gradle", props
+
+// build.gradle
 // Lazybones tries to fine a value for $version if this is left in the build.gradle. This is to get it to stop complaining.
 props.versionString = "\$version\${project.hasProperty('release') ? '' : '-SNAPSHOT'}"
 props.minSdkVersion = ask("Define the minimum Android SDK version this library should support [7]: ", "7", "minSdkVersion")
 
 processTemplates "build.gradle", props
+
+// gradle.properties
+props.version = ask("Define the version number [1.0.0]: ", "1.0.0", "version")
+props.group = ask("Define the group [com.example]: ", "com.example", "group")
+props.description = ask("Define the description of this project [An example project]: ", "An example project", "description")
+
+processTemplates "gradle.properties", props
